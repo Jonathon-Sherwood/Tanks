470ae76b6abc3d9f0c6a4c3e484c766cdce64c8a
Jonathon <jonathondsherwood@hotmail.com>
2020-11-06 15:15:06 -0700

    Random Player Spawning

--- Assets/Scripts/Constants/GameManager.cs
+++ Assets/Scripts/Constants/GameManager.cs
@@ -10,6 +10,7 @@ public class GameManager : MonoBehaviour
     public List<HumanController> humanPlayers;
     public List<AIController> aiPlayers;
     public List<ShipData> shipList; //This list is attached to ship objects that will fill this list.
+    public GameObject[] playerSpawnPoints;
     private int pauseTime = 10;
     private float pauseCountdown;
     private bool pauseOver = true;
@@ -59,12 +60,11 @@ public class GameManager : MonoBehaviour
 
     public void RespawnPlayer() //Hooks the newly spawned ship to the human controller
     {
-        playerShipData = Instantiate(playerPrefab, new Vector3(0, 9, 0), Quaternion.identity).gameObject.GetComponent<ShipData>();
-        //playerShipData.mover = humanPlayers[0].mover.GetComponent<ShipMover>();
-        //playerShipData.shooter = humanPlayers[0].shooter.GetComponent<ShipShooter>();
+        playerSpawnPoints = GameObject.FindGameObjectsWithTag("PlayerSpawn");
+        Transform spawnLocation = playerSpawnPoints[Random.Range(0, playerSpawnPoints.Length)].transform;
+        playerShipData = Instantiate(playerPrefab, spawnLocation.position, Quaternion.identity).gameObject.GetComponent<ShipData>();
         playerShipData.owner = humanPlayers[0].gameObject;
         humanPlayers[0].mover = playerShipData.mover.GetComponent<ShipMover>();
         humanPlayers[0].shooter = playerShipData.shooter.GetComponent<ShipShooter>();
-
     }
 }
