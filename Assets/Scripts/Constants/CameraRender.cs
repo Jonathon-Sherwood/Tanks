d016f4658e1afb8018e683a4bee7876a73f07829
Jonathon Sherwood <jonathondsherwood@hotmail.com>
2020-12-15 15:58:43 -0700

    Fog and lighting

---
+++ Assets/Scripts/Constants/CameraRender.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRender : MonoBehaviour
+{
+
+    //Stops this camera from processing fog
+    private void OnPreRender()
+    {
+        RenderSettings.fog = false;
+    }
+
+    //Turns back on the fog for the rest of the game
+    private void OnPostRender()
+    {
+        RenderSettings.fog = true;
+    }
+}
